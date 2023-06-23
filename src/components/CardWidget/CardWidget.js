5c7bdd963537b43f2446532ea6761d48b4a7f39b
Nahuel Tello <67810116+NahuelTello@users.noreply.github.com>
2023-06-22 22:20:17 -0300

    cart

--- src/components/CardWidget/CardWidget.js
+++ src/components/CardWidget/CardWidget.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import img from "../../assets/img/cart2.svg"
+import './CardWidget.css'
 const CardWidget = () => {
   return (
-      <>
-          <div className="card-widget">
-              <img src={img} alt="cart" />
-              <p>
-                  <span>2</span>
-              </p>
-          </div>
-      </>
+    <>
+        <div className="card-widget">
+            <img src={img} alt="cart" />
+            <p>
+                <span>2</span>
+            </p>
+        </div>
+    </>
   )
 }
 
